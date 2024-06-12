98ee9d68496006f6405d428df124d77fbc45898a
ASWIN A K <007aswinak007@gmail.com>
2024-06-12 19:50:46 +0530

    commit for intial deployment 3 and added db.json

--- FullStackPortfolioBackend/settings.py
+++ FullStackPortfolioBackend/settings.py
@@ -86,9 +86,22 @@ WSGI_APPLICATION = 'FullStackPortfolioBackend.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
-}
+# DATABASES = {
+#     'default': dj_database_url.config(conn_max_age=600)
+# }
+
+# Database configuration
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
