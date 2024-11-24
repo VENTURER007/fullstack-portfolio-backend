c1793307cecfd19cb1de292c552a760fadcbf29e
ASWIN A K <007aswinak007@gmail.com>
2024-11-24 13:00:33 +0530

    Updated MEDIA_ROOT

--- FullStackPortfolioBackend/settings.py
+++ FullStackPortfolioBackend/settings.py
@@ -167,8 +167,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #media configuration
-MEDIA_URL = '/assets/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 
 
 
